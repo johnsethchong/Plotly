6c6de71a4998e56ca231746e50c774b260964a53
Johnseth Chong <johnsethchong@gmail.com>
2019-12-10 21:37:15 -0800

    Minor change

--- Starter_Code/static/js/app.js
+++ Starter_Code/static/js/app.js
@@ -1,14 +1,14 @@
-// function buildPlot() {
-//     d3.jason("Starter_Code/samples.json").then(function(data) {
-//         var selector = d3.select("#sample.metadata");
-//         selector.html("");
-//         Object.entries(data).forEach(([key,value]) => {
-//             selector.append().text(`${key}: ${value}`);
-//         });
-//         console.log(data);
-//     })
+function buildPlot() {
+    d3.jason("samples.json").then(function(data) {
+        var selector = d3.select("#sample.metadata");
+        selector.html("");
+        Object.entries(data).forEach(([key,value]) => {
+            selector.append("p").text(`${key}: ${value}`);
+        });
+        console.log(data);
+    })
 
-// }
+}
 
 
 d3.json("samples.json").then(function(data) {
