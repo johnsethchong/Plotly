acd1eeb42f773d01e693e72027d19f02f37b7bf3
Johnseth Chong <johnsethchong@gmail.com>
2019-12-13 07:10:23 -0800

    update

--- Starter_Code/static/js/app.js
+++ Starter_Code/static/js/app.js
@@ -6,11 +6,50 @@ function buildPlot() {
             selector.append("p").text(`${key}: ${value}`);
         });
         console.log(data);
-    })
-
+    });
 }
 
+function buildCharts() {
+    d3.json("samples.json").then(function(data){
+        var values = data.sample_values.slice(0,10);
+        var labels = data.otu_ids.slice(0,10);
+        var hoverText = data.otu_labels.slice(0,10);
+        
+        var data = [{
+            type: "bar",
+            x: values,
+            y: labels,
+            hovertext: hoverText,
+            orientation: "h"
+        }];
+        Plotly.newPlot("bar", data);
+    };
 
-d3.json("samples.json").then(function(data) {
-    console.log(data);
-});
+    d3.json("samples.json").then(function(data){
+        var x_values = data.otu_ids;
+        var y_values = data.sample_values;
+        var markerSize = data.sample_values;
+        var markerColor = data.otu_ids;
+        var textValues = data.otu_labels;
+
+        var trace1 = {
+            x: x_values,
+            y: y_values,
+            mode: "markers",
+            marker: {
+                color: markerColor,
+                size: markerSize
+            },
+            text: textValues
+        };
+
+        var data = [trace1];
+        var layout = {title: "OTU ID"};
+
+        Plotly.newPlot("bubble", data, layout);
+    });
+}
+
+function init() {
+    var selector = d3.select("#selDataset");
+}
