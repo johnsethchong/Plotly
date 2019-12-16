7508947557ed80f7ce9db35f5e76daad2bd03266
Johnseth Chong <johnsethchong@gmail.com>
2019-12-16 13:21:15 -0800

    edit

--- Starter_Code/static/js/app.js
+++ Starter_Code/static/js/app.js
@@ -1,7 +1,9 @@
 function buildPlot() {
     d3.jason("samples.json").then(function(data) {
+        //select #sample.metadata
         var selector = d3.select("#sample.metadata");
         selector.html("");
+        //add each key-value pair
         Object.entries(data).forEach(([key,value]) => {
             selector.append("p").text(`${key}: ${value}`);
         });
@@ -10,7 +12,9 @@ function buildPlot() {
 }
 
 function buildCharts() {
+   //build horizontal bar chart
     d3.json("samples.json").then(function(data){
+        //use slice to get top 10 values
         var values = data.sample_values.slice(0,10);
         var labels = data.otu_ids.slice(0,10);
         var hoverText = data.otu_labels.slice(0,10);
@@ -25,13 +29,15 @@ function buildCharts() {
         Plotly.newPlot("bar", data);
     };
 
+    //build bubble chart
     d3.json("samples.json").then(function(data){
         var x_values = data.otu_ids;
         var y_values = data.sample_values;
         var markerSize = data.sample_values;
         var markerColor = data.otu_ids;
         var textValues = data.otu_labels;
-
+        
+        //create trace
         var trace1 = {
             x: x_values,
             y: y_values,
